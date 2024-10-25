d919d35fdabb63b8b3e98ca1852c28c474ecb2e5
zlemura <zlemura29@gmail.com>
2024-10-25 13:02:12 +1100

    Add files via upload
    Added source_to_checklist.py
    
    Working to fetch source_product, convert to pickle files, convert to checklist_object, convert to pickle files.

--- main.py
+++ main.py
@@ -3,35 +3,30 @@ import time
 import random
 
 import source as source
+import source_to_checklist as source_to_checklist
 import pickle_actions as pickle
 
-def run():
-    '''
+def fetch_set_lists_for_categories():
+        category_identifiers = ["baseball-cards", "basketball-cards", "football-cards", "hockey-cards", "soccer-cards",
+                                "racing-cards", "wrestling-cards", "ufc-cards"]
+        # Write set lists for categories.
+        for category_identifier in category_identifiers:
+            source_set_list = source.fetch_all_set_identifiers_for_category(category_identifier)
+            pickle.write_to_dump_file(source_set_list, "dumps/category_set_lists/" + category_identifier)
+            sleep_time = random.randrange(1,6)
+            print("Processed " + category_identifier + ". Sleeping for " + sleep_time.__str__() + " seconds.")
+            time.sleep(sleep_time)
+
+        # Read set lists for categories.
+        for category_identifier in category_identifiers:
+            set_results = pickle.open_dump_file("dumps/category_set_lists/" + category_identifier)
+            for result in set_results:
+                print(result.__dict__)
+            print(category_identifier + " has " + len(set_results).__str__() + " set results.")
+
+def fetch_set_lists_for_category():
     category_identifiers = ["baseball-cards", "basketball-cards", "football-cards", "hockey-cards", "soccer-cards",
-                            "racing-cards", "wrestling-cards", "ufc-cards"]
-    # Write set lists for categories.
-    for category_identifier in category_identifiers:
-        source_set_list = source.fetch_all_set_identifiers_for_category(category_identifier)
-        pickle.write_to_dump_file(source_set_list, "dumps/category_set_lists/" + category_identifier)
-        sleep_time = random.randrange(1,6)
-        print("Processed " + category_identifier + ". Sleeping for " + sleep_time.__str__() + " seconds.")
-        time.sleep(sleep_time)
-
-    # Read set lists for categories.
-    for category_identifier in category_identifiers:
-        set_results = pickle.open_dump_file("dumps/category_set_lists/" + category_identifier)
-        for result in set_results:
-            print(result.__dict__)
-        print(category_identifier + " has " + len(set_results).__str__() + " set results.")
-    '''
-
-    # Serialized data - serialized as a tuple.
-    ## {results, setName}
-    # Fetch list of all set_identifiers for each sport.
-    # Take dumps and convert to checklist_object.
-    # Serialize and save checklist_objects to dumps/checklist_objects/catgory_identifier
-
-    category_identifiers = ["football-cards"]
+                                "racing-cards", "wrestling-cards", "ufc-cards"]
 
     for category_identifier in category_identifiers:
         category_set_list = pickle.open_dump_file("dumps/category_set_lists/" + category_identifier)
@@ -42,12 +37,26 @@ def run():
             file_name_for_set = "dumps/" + category_identifier + "/" + set_identifier.href + ".pkl"
             if os.path.isfile(file_name_for_set) == True:
                 print("File exists for " + set_identifier.href)
-                counter+= 1
+                counter += 1
                 continue
             set_product_list = source.fetch_set_results(set_identifier.href)
             pickle.write_to_dump_file(set_product_list, file_path_for_set)
             counter += 1
             print("Finished processing " + counter.__str__() + " of " + len(category_set_list).__str__())
+
+def convert_category_source_products_to_checklist_objects():
+    category_identifiers = ["baseball-cards", "basketball-cards", "football-cards", "hockey-cards", "soccer-cards",
+                                    "racing-cards", "wrestling-cards", "ufc-cards"]
+    for category in category_identifiers:
+        print("Processing ", category)
+        source_to_checklist.convert_set_products_to_checklist_objects_by_category(category)
+        print("Finished processing", category)
+
+def run():
+    # create database table files in CSV for data.
+    # tables outlined in Google Doc.
+    print("Hi")
+
 
 if __name__ == '__main__':
     run()
--- source_to_checklist.py
+++ source_to_checklist.py
@@ -1,11 +1,41 @@
+import os
+
+from checklist_object import checklist_object
+import pickle_actions as pickle
+
+def convert_set_products_to_checklist_objects_by_category(category_identifier):
+    category_file_list = fetch_category_set_files(category_identifier)
+
+    for file in category_file_list:
+        set_source_product_list, set_name = pickle.open_dump_file("dumps/" + category_identifier + "/" + file.replace(".pkl",""))
+        set_checklist_objects = convert_source_products_to_checklist_objects(set_source_product_list,set_name)
+        pickle.write_to_dump_file(set_checklist_objects,"checklists/" + category_identifier + "/" + file.replace(".pkl",""))
+
+def fetch_category_set_files(category_identifier):
+    return os.listdir("dumps/" + category_identifier)
+
+def fetch_category_checklist_files(category_identifier):
+    return os.listdir("checklists/" + category_identifier)
 
 def convert_source_products_to_checklist_objects(source_products, set_name):
     checklist_objects = []
     for product in source_products:
+        variantName = ""
+        printRun = product.printRun
+        setName = set_name
+        if "[" in product.productName and "]" in product.productName:
+            variantName = product.productName.split('[')[1].split(']')[0]
+            playerName = product.productName.split("[")[0].strip()
+            cardNumber = product.productName.split("]")[1].strip()
+            #print("VARIANT FOUND: ",playerName, "-", variantName, "-", cardNumber, "-", printRun, "-", setName)
+            checklist_objects.append(checklist_object(playerName, variantName, cardNumber, printRun, setName))
+        else:
+            product_name_split_by_hash = product.productName.split("#")
+            product_name_split_by_name = product.productName.split(product_name_split_by_hash[0])
 
-# Check if source_product.productName contains [].
-## If so, get all text before [ and strip() = Player name.
-## Get all values between [] and strip() = Variant name.
-# Get values connected to # and strip() = card number.
-# source_product.printRun = print run.
-# set_name = set name.
+            playerName = product_name_split_by_hash[0].strip()
+            cardNumber = product_name_split_by_name[1].strip()
+            #print("VARIANT NOT FOUND: ",playerName, "-", variantName, "-", cardNumber, "-", printRun, "-", setName)
+            checklist_objects.append(checklist_object(playerName, variantName, cardNumber, printRun, setName))
+
+    return checklist_objects
