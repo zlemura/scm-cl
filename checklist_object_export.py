fe190915683704fe426c1a8a447a352908bdc3ae
zlemura <zlemura29@gmail.com>
2024-10-31 20:29:08 +1100

    Add files via upload

--- checklist_object_export.py
+++ checklist_object_export.py
@@ -2,10 +2,18 @@ import pickle_actions as pickle
 
 import csv
 import os
-import time
 
 def export_category_checklist_objects(category):
     category_objects = fetch_all_objects_for_category(category)
+
+    create_set_files_for_category(category)
+
+    # create_set_files_for_category
+    # create_player_name_files_for_category
+    # create_variant_files_for_category
+    # create_print_run_files_for_category
+    # create_year_files_for_category
+    # create_enum_files_for_category
 
 def fetch_all_objects_for_category(category):
     category_set_file_list = os.listdir("checklists/" + category)
@@ -21,8 +29,33 @@ def fetch_all_objects_for_category(category):
 def create_set_files_for_category(category):
     category_set_file_list = os.listdir("checklists/" + category)
 
-    for file in category_set_file_list:
-        file_objects = pickle.open_dump_file("checklists/" + category + "/" + file.replace(".pkl", ""))
+    for file_name in category_set_file_list:
+        print("Processing ", file_name)
+        file_objects = pickle.open_dump_file("checklists/" + category + "/" + file_name.replace(".pkl", ""))
+        objects_to_add_to_file = []
+        for object in file_objects:
+            objects_to_add_to_file.append(convert_checklist_object_to_row(object))
+        write_to_csv_file("exports/sets/" + category + "/" + file_name.replace(".pkl","") + ".csv", objects_to_add_to_file)
+        print("Processed ", file_name)
+
+
+def convert_checklist_object_to_row(checklist_object):
+    year = fetch_year_from_set_name(checklist_object.setName)
+    manufacturer = fetch_manufacturer_from_set_name(checklist_object.setName)
+    playerName = checklist_object.playerName
+    characters_to_remove = [("ō", "o")]
+    for character in characters_to_remove:
+        if character[0] in playerName:
+            playerName = playerName.replace(character[0], character[1])
+
+    '''
+    print(playerName, checklist_object.variantName, checklist_object.cardNumber, \
+           checklist_object.printRun, checklist_object.setName, year, manufacturer)
+    '''
+
+    return playerName.strip(), checklist_object.variantName.strip(), checklist_object.cardNumber.strip(), \
+           checklist_object.printRun, checklist_object.setName.strip(), year, manufacturer.strip()
+
 
 def fetch_year_from_set_name(setName):
     year = int(setName.split(" ")[0])
@@ -42,3 +75,18 @@ def fetch_manufacturer_from_set_name(setName):
     for manufacturer in manufacturer_list:
         if manufacturer in setName:
             return manufacturer
+
+def write_to_csv_file(file_name, objects_to_write):
+    with open(file_name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile,  delimiter='|')
+        writer.writerows(objects_to_write)
+
+class object_csv_structure:
+    def __init__(self, playerName, variantName, cardNumber, printRun, setName, setYear, setManufacturer):
+        self.playerName = playerName
+        self.variantName = variantName
+        self.cardNumber = cardNumber
+        self.printRun = printRun
+        self.setName = setName
+        self.setYear = setYear
+        self.setManufacturer = setManufacturer
