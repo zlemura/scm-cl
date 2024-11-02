051e62d5510d1f21f460c53ca3fec123f28aca58
zlemura <zlemura29@gmail.com>
2024-11-02 18:28:39 +1100

    Add files via upload

--- checklist_object_export.py
+++ checklist_object_export.py
@@ -25,6 +25,8 @@ def export_category_checklist_objects(category):
 
     #create_variants_enums_list(category)
 
+    #create_set_names_enum_list(category)
+
 def fetch_all_objects_for_category(category):
     category_set_file_list = os.listdir("checklists/" + category)
 
@@ -305,7 +307,12 @@ def create_player_names_enums_list(category):
     for enum in enum_list_values:
         enums_to_add_to_file.append((enum[0],enum[1]))
 
-    write_to_csv_file("exports/enums/player_names.csv", enums_to_add_to_file)
+    try:
+        os.mkdir("exports/enums/" + category)
+    except FileExistsError:
+        print("Directory exists for ", category)
+
+    write_to_csv_file("exports/enums/" + category + "/player_names.csv", enums_to_add_to_file)
 
 def create_variants_enums_list(category):
     enum_list_values = []
@@ -333,7 +340,42 @@ def create_variants_enums_list(category):
     for enum in enum_list_values:
         enums_to_add_to_file.append((enum[0], enum[1]))
 
-    write_to_csv_file("exports/enums/variants.csv", enums_to_add_to_file)
+    try:
+        os.mkdir("exports/enums/" + category)
+    except FileExistsError:
+        print("Directory exists for ", category)
+
+    write_to_csv_file("exports/enums/" + category + "/variants.csv", enums_to_add_to_file)
+
+def create_set_names_enum_list(category):
+    enum_list_values = []
+
+    set_names_file_path = "exports/sets/"
+    category_set_names_directory_names = os.listdir(set_names_file_path + category)
+    for category_set_file_name in category_set_names_directory_names:
+        first_record = get_first_record_in_csv(set_names_file_path + category + "/" + category_set_file_name)
+        if len(first_record) > 1:
+            combined_first_record = ''
+            for record in first_record:
+                if len(record) == 0:
+                    continue
+                combined_first_record += record
+            first_record_split = combined_first_record.split("|")
+        else:
+            first_record_split = first_record[0].split("|")
+        if len(first_record_split[4]) > 0:
+            enum_list_values.append([first_record_split[4], category_set_file_name.replace(".csv", "")])
+
+    enums_to_add_to_file = []
+    for enum in enum_list_values:
+        enums_to_add_to_file.append((enum[0], enum[1]))
+
+    try:
+        os.mkdir("exports/enums/" + category)
+    except FileExistsError:
+        print("Directory exists for ", category)
+
+    write_to_csv_file("exports/enums/" + category + "/sets.csv", enums_to_add_to_file)
 
 
 def convert_checklist_object_to_row(checklist_object):
